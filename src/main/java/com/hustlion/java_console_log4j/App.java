8896b91bc28f3795025bdc015829d9123112ebe9
HustLion <hustlionm@qq.com>
2017-05-27 22:32:41 +0800

    project added

---
+++ src/main/java/com/hustlion/java_console_log4j/App.java
@@ -0,0 +1,20 @@
+package com.hustlion.java_console_log4j;
+
+//引入 log4j 类
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	// 定义对 rootLogger 的静态引用
+	private static final Logger rootLogger = LogManager.getRootLogger();
+
+	public static void main(String[] args) {
+		rootLogger.error("Logged by root logger: Hello this is an error");
+		rootLogger.info("Logged by root logger: Hello World!");
+		rootLogger.debug("Logged by root logger: Hello debug message.");
+	}
+}
